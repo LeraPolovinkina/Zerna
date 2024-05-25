e795eb130ae521142dbe2936d2c070c3c14fc827
LightGrey <92320503+LeraPolovinkina@users.noreply.github.com>
2024-05-25 12:18:12 +0600

    code cleaning

--- zerna/views.py
+++ zerna/views.py
@@ -16,7 +16,6 @@ def upload_image(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     elif request.method == 'POST':
-        # Добавьте здесь код для обработки загрузки изображения
         return HttpResponse("Image uploaded successfully!")
 
 def predict_one_sample(model, input_tensor):
